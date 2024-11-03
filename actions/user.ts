f0bddc1e2435f2509b3c599459bbc05fece0004c
Abhiram <vemulaabhiram2004@gmail.com>
2024-11-03 15:12:09 +0530

    add actions to check and create user profile in leaderboard

--- actions/user.ts
+++ actions/user.ts
@@ -3,20 +3,103 @@
 import { prisma } from "@/prisma";
 
 export const getUserfromEmail = async (email: string) => {
-    const user = await prisma.user.findUnique({
-        where:{
-            email:email,
-        }
-    })
-    return user?.id;
-}
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  return user?.id;
+};
 
 export const getLeaderboardProfile = async (email: string) => {
-    const id = await getUserfromEmail(email);
-    const profile = await prisma.leaderboard.findUnique({
-        where:{
-            user_id:id,
-        },
-    })
-    return profile;
-}
+  const id = await getUserfromEmail(email);
+  const profile = await prisma.leaderboard.findUnique({
+    where: {
+      user_id: id,
+    },
+  });
+  return profile;
+};
+
+export const checkUserProfile = async ({
+  username,
+  codechefusername,
+  leetcodeusername,
+  codeforcesusername,
+}: {
+  username: string;
+  codeforcesusername: string;
+  leetcodeusername: string;
+  codechefusername: string;
+}) => {
+  try {
+    const exusername = await prisma.leaderboard.findUnique({
+      where: {
+        username,
+      },
+    });
+    const excodechefusername = await prisma.leaderboard.findUnique({
+      where: {
+        codechefusername,
+      },
+    });
+    const excodeforcesusername = await prisma.leaderboard.findUnique({
+      where: {
+        codeforcesusername,
+      },
+    });
+    const exleetcodeusername = await prisma.leaderboard.findUnique({
+      where: {
+        leetcodeusername,
+      },
+    });
+
+    const obj: {
+      username: boolean;
+      leetcodeusername: boolean;
+      codeforcesusername: boolean;
+      codechefusername: boolean;
+    } = {
+      username: false,
+      leetcodeusername: false,
+      codechefusername: false,
+      codeforcesusername: false,
+    };
+    if (exusername) obj.username = true;
+    if (exleetcodeusername) obj.leetcodeusername = true;
+    if (excodeforcesusername) obj.codeforcesusername = true;
+    if (excodechefusername) obj.codechefusername = true;
+
+    return obj;
+  } catch (error) {
+    console.log("Error actions/user.ts > checkUserProfile", error);
+  }
+};
+
+export const createProfile = async ({
+  user_id,
+  username,
+  codechefusername,
+  leetcodeusername,
+  codeforcesusername,
+}: {
+  user_id: string;
+  username: string;
+  codechefusername: string;
+  leetcodeusername: string;
+  codeforcesusername: string;
+}) => {
+  try {
+    await prisma.leaderboard.create({
+      data: {
+        user_id,
+        username,
+        codechefusername,
+        codeforcesusername,
+        leetcodeusername,
+      },
+    });
+  } catch (error) {
+    console.log("Error actions/user.ts > createProfile", error);
+  }
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/sonner";
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,9 +16,7 @@ export default function RootLayout({
   return (
     <SessionProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body>
           {children}
           <Toaster />
         </body>

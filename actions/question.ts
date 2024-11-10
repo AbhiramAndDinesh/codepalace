d8dfb9123ad4156fec12b6744f47b6b21d71d0e7
dinesh-p0 <dineshkrishna0456@gmail.com>
2024-11-10 17:23:36 +0530

    getquestion action changes

--- actions/question.ts
+++ actions/question.ts
@@ -3,7 +3,14 @@ import { prisma } from "@/prisma";
 
 export const getQuestions = async () => {
   try {
-    const questions = await prisma.problem.findMany({});
+    const questions = await prisma.problem.findMany({
+      where: {
+        inCourse: false,
+      },
+      orderBy: {
+        problem_id: "asc",
+      },
+    });
     return questions;
   } catch (error) {
     console.log("Error in actions/question.ts > getQuestion", error);

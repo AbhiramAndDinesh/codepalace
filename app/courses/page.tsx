564b0ce696e19064327c23ba362df673e76e077f
dinesh-p0 <dineshkrishna0456@gmail.com>
2024-11-11 00:22:02 +0530

    usercourses and explore courses unique

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -4,29 +4,30 @@ import { auth } from "@/auth";
 export default async function CoursePage() {
   const session = await auth();
   const courses = await getCourses();
-  if (!session?.user) return <div>Not signed in</div>;
+  if (!session) return;
+  if (!session.user) return <div>Not signed in</div>;
   if (!courses) return <div>Loading</div>;
-  const usercourses = await getUserCourses(session.user.id);
+  const user_id = session.user.id || "";
+  const usercourses = await getUserCourses(user_id);
+  const ids = new Map(usercourses?.map((course) => [course.course_id, true]));
+  const courses_ = courses.filter((course) => !ids.has(course.course_id));
   console.log(usercourses);
-  const courses_ = courses.filter(
-    (cour) =>
-      !usercourses.some((cours) => cour.course_id === cours.course.course_id),
-  );
   return (
     <div className="min-h-screen min-w-[100vw] p-10">
       <div>
         <h1>My coursese</h1>
-        {usercourses.map((course) => (
-          <Link
-            key={course.course_id}
-            className=""
-            href={`/courses/${course.course.slug}`}
-          >
-            <div className="min-w-[700px] min-h-[20%] rounded-md shadow-md p-3">
-              {course.course.name}
-            </div>
-          </Link>
-        ))}
+        {usercourses &&
+          usercourses.map((course) => (
+            <Link
+              key={course.course_id}
+              className=""
+              href={`/courses/${course.course.slug}`}
+            >
+              <div className="min-w-[700px] min-h-[20%] rounded-md shadow-md p-3">
+                {course.course.name}
+              </div>
+            </Link>
+          ))}
       </div>
       <div>
         <h1>Explore courses</h1>
